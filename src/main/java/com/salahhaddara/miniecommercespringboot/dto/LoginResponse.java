1e111158368c81f9aa7fddff64399e91f51ac113
Salah Haddara <Salahhaddara02@gmail.com>
2025-09-11 22:21:14 +0300

    Add LoginResponse DTO

---
+++ src/main/java/com/salahhaddara/miniecommercespringboot/dto/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.salahhaddara.miniecommercespringboot.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class LoginResponse {
+
+    private String token;
+    private String email;
+    private String role;
+}
+
