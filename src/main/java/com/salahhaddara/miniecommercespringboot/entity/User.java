9bd67883fc8ca925ebeadee1038ecb3c9318d75c
Salah Haddara <Salahhaddara02@gmail.com>
2025-09-11 16:59:10 +0300

    Add User entity

---
+++ src/main/java/com/salahhaddara/miniecommercespringboot/entity/User.java
@@ -0,0 +1,30 @@
+package com.salahhaddara.miniecommercespringboot.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank
+    @Size(min = 6)
+    @Column(nullable = false)
+    private String password;
+}
