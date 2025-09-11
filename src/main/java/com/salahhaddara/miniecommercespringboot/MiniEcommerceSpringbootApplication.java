70eeace6e63989bef1615816960642ab03853462
Salah Haddara <Salahhaddara02@gmail.com>
2025-09-11 12:42:12 +0300

    Add project setup

---
+++ src/main/java/com/salahhaddara/miniecommercespringboot/MiniEcommerceSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.salahhaddara.miniecommercespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniEcommerceSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniEcommerceSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salahhaddara/miniecommercespringboot/MiniEcommerceSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salahhaddara.miniecommercespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniEcommerceSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
